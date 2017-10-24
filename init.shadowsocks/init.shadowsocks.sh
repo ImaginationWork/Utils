ea3335e43145367afeffc07e99675f84b6bcc9de
ImaginationWork <3290639239@qq.com>
2017-10-24 20:07:54 +0800

    Update init.shadowsocks.sh

--- init.shadowsocks/init.shadowsocks.sh
+++ init.shadowsocks/init.shadowsocks.sh
@@ -2,5 +2,5 @@
 # please use root permission to run this
 export LC_ALL=C
 apt install python-pip -y
-pip install shadowsocks
+pip install setuptools shadowsocks
 ssserver -p 8388 -k password -m rc4-md5 -d start
